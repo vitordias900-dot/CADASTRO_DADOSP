665c61ffa582f1287774196cd3a4b6566e9d8f15
vitordias900-dot <vitordias900@gmail.com>
2026-02-02 10:37:27 -0300

    Implementa cadastro de múltiplas pessoas

--- CADASTRO_DADOS/Program.cs
+++ CADASTRO_DADOS/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            List<string> nomes = new List<string>();
+            List<int> idades = new List<int>();
+            List<double> salarios = new List<double>();
+            List<char> sexos = new List<char>();
+
+            char opcao;
+
+            Console.WriteLine("----- CADASTRO -----");
+
+            do
+            {
+                Console.Write("Nome: ");
+                nomes.Add(Console.ReadLine());
+
+                Console.Write("Idade : ");
+                idades.Add(int.Parse(Console.ReadLine()));
+
+                Console.Write("Salario : ");
+                salarios.Add(double.Parse(Console.ReadLine(), CI));
+
+                Console.Write("Digite o sexo (M/F): ");
+                sexos.Add(char.Parse(Console.ReadLine()));
+
+                Console.Write("\nDeseja cadastrar outra pessoa? (s/n): ");
+                opcao = char.Parse(Console.ReadLine().ToLower());
+
+                Console.WriteLine();
+
+            } while (opcao == 's');
+
+            Console.WriteLine("---- CADASTROS REALIZADOS ----\n");
+
+            for (int i = 0; i < nomes.Count; i++)
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine($"Nome   : {nomes[i]}");
+                Console.WriteLine($"Idade  : {idades[i]} anos");
+                Console.WriteLine($"Salário: R$ {salarios[i].ToString("F3", CI)}");
+                Console.WriteLine($"Sexo   : {sexos[i]}");
+                Console.WriteLine();
+            }
+
+        }
+    }
+    
+}
